f6eb7fec64c446eaa76d7b5fd3a5887dc8bf6498
Mayo <mayo@linux.com>
2024-06-10 00:26:31 +0000

    add content of note

--- scripts/note.py
+++ scripts/note.py
@@ -1,4 +1,7 @@
 import feedparser
+import requests
+from bs4 import BeautifulSoup
+from markdownify import markdownify
 from datetime import datetime
 
 # Read URL from .note file
@@ -21,18 +24,32 @@ for entry in feed.entries:
     parsed_date = datetime.strptime(date, '%a, %d %b %Y %H:%M:%S %z')
     formatted_date = parsed_date.strftime('%Y-%m-%d')
 
+    # Fetch the HTML content from the entry URL
+    response = requests.get(entry_url)
+    html_content = response.content
+
+    # Parse the HTML content
+    soup = BeautifulSoup(html_content, 'html.parser')
+    content_div = soup.find(class_='note-common-styles__textnote-body')
+
+    # Convert the HTML content to Markdown
+    if content_div:
+        markdown_content = markdownify(str(content_div))
+    else:
+        markdown_content = "Content not found."
+
     # Format title and content
     formatted_title = title.replace(" ", "-").lower()
-    formatted_content =f"""---
+    formatted_content = f"""---
 title: "{title}"
 date: "{formatted_date}"
-externalLink: "{entry_url}"
+toc: false
+typeface: serif
 ---
 
-This is the content for the note titled "{title}" published on {formatted_date}.
+> This is the content for the note titled "{title}" published on [{entry_url}]({entry_url}).
 
-Read more at: {entry_url}
-"""
+{markdown_content}"""
 
     # Save the formatted content to a file
     filename = f"./content/posts/note/{formatted_title}.md"
