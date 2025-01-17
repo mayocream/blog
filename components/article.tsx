ddfd74d3173758a43c0e52de9d2f1daff036c48a
Mayo <mayo@linux.com>
2025-01-18 05:38:45 +0900

    feat: Add mermaid dependency and initialize rendering for mermaid diagrams in articles

--- components/article.tsx
+++ components/article.tsx
@@ -2,10 +2,23 @@
 
 import { useEffect } from 'react'
 import hljs from 'highlight.js'
+import mermaid from 'mermaid'
 
 export default function Article({ raw }: { raw: string }) {
   useEffect(() => {
     hljs.highlightAll()
+    mermaid.init({ startOnLoad: true }, 'pre code.language-mermaid', () => {
+      const codeBlocks = document.querySelectorAll('code.language-mermaid')
+      for (const codeBlock of codeBlocks) {
+        let element = (codeBlock as HTMLElement)
+        element.style.backgroundColor = 'initial'
+
+        const preBlock = (element.parentNode as HTMLElement)
+        preBlock.style.border = 'none'
+        preBlock.style.textAlign = 'center'
+        preBlock.style.backgroundColor = 'initial'
+      }
+    })
   }, [])
 
   return <article className="prose lg:prose-xl" dangerouslySetInnerHTML={{ __html: raw }} />
